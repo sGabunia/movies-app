7c9359d6cfa440d3698117b5a31659b7e605cea6
Sergo Gabunia <sergogabunia@gmail.com>
2021-09-08 22:44:59 +0400

    created slider component, used swiper.js library

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
 import { useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+
+import "swiper/swiper.min.css";
+import "swiper/swiper-bundle.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+
+import "./Slider.css";
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 const Slider = () => {
   const movies = useSelector(({ movies }) => movies);
-  console.log(movies);
 
   return (
-    <div>
+    <section>
       <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        modules={[Pagination, Navigation, Autoplay]}
+        spaceBetween={0}
+        autoplay={{
+          delay: 3500,
+        }}
+        pagination={{ clickable: true }}
+        loop={true}
+        initialSlide={1}
+        effect="flip"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {movies.slice(0, 8).map((movie) => (
+          <SwiperSlide key={movie.id}>
+            <div className="poster-info">
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                alt="poster"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
-    </div>
+    </section>
   );
 };
 
